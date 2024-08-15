6f41a84578a7508e669c74f0dc707b1c82c7d4ca
martin <tiin.-@hotmail.es>
2024-08-15 16:27:30 -0300

    diseño de header terminado

--- src/componnets/Header/Header.jsx
+++ src/componnets/Header/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-[#1d2125]'>Header</div>
+    <div className='bg-[#1d2125] w-100 h-12 p-3 border-b border-box flex flex-grow justify-between border-b-[#9fadbc26]'>
+     <div className="left justify-center items-center flex">
+     <h3 className='text-slate-50'>Gestion de Tareas</h3>
+     </div>
+     <div className="right flex items-center space-x-4">
+      <span>Remote dev</span>
+      <img className='rounded-full w-10' src="https://dreamlightvalleywiki.com/images/6/6a/Mike_Wazowski.png" alt="perfil dev" />
+     </div>
+      </div>
   )
 }
 
--- src/componnets/Main/Main.jsx
+++ src/componnets/Main/Main.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { MoreHorizontal, UserPlus } from 'react-feather'
 
 const Main = () => {
   return (
-    <div className='flex flex-col bg-slate-900 w-full'>Main</div>
+    <div className='flex flex-col bg-slate-900 w-full'>
+      <div className="p-3 bg-black flex justify-between w-full bg-opacity-20">
+        <h2 className='text-lg'> Mi tablero</h2>
+        <div className='flex items-center justify-center'>
+          <button className='bg-gray-200 text-gray-500 px-2 py-1 mr-2 rounded flex justify-center items-center'><UserPlus className='mr-2' size={16}></UserPlus> Share</button>
+          <button className='hover:bg-slate-500   h-8 px-2 py-1 rounded'><MoreHorizontal size={16} /></button>
+        </div>
+      </div>
+      <div className='flex flex-col w-full flex-grow'>
+        <div className='mr-3 w-60 h-fit rounded-md p-2 bg-black'>
+          <div className="list-body">
+            <div className='flex justify-between'>
+              <span>To do</span>
+              <button><MoreHorizontal size={16}></MoreHorizontal></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
